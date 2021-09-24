4f88fcc6a8ea7b6c7d6d2b881735ed233f074322
Kamil Flak <kamilflak@MacBook-Pro-Kamil.local>
2021-09-24 15:19:58 +0200

    test commit

---
+++ window-object.js
@@ -0,0 +1,9 @@
+var msg ='<h2> okno przegladarki</h2><p>szerokość: ' + window.innerWidth + '</p>';
+msg += '<p>wysokość: ' + window.innerHeight + '</p>';
+msg += '<h2>historia</h2><p>elementy: ' + window.history.lenght + '</p>';
+msg += '<h2>erkan</h2><p>szerokosc: ' + window.screen.width + '</p>';
+msg += '<p>wysokość: ' + window.screen.height + '</p>';
+
+var el = document.getElementById('info');
+el.innerHTML = msg;
+alert('bieąca strona: ' + window.location);
